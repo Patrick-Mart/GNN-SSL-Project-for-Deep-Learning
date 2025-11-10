d3aa831a8f364d20d82f2c9fbafcee1058a57acd
Patrick Martins <patrick.j.martins@hotmail.com>
2025-11-10 11:49:46 +0100

    added git ignore file and dependencies list

--- data.py
+++ data.py
@@ -1,12 +1,24 @@
 import torch
 from torch_geometric.datasets import OGB_MAG
-from torch_geometric.loader import DataLoader 
+from torch_geometric.loader import NeighborLoader
 
-batch_size = 16
 
-data = OGB_MAG('../')
-loader = DataLoader(data, batch_size=batch_size, shuffle=True)
+hetero_data = OGB_MAG('../')[0]
 
-for batch in loader:
-    print(batch)
+loader = NeighborLoader(
+    hetero_data,
+    # Sample 30 neighbors for each node and edge type for 2 iterations
+    num_neighbors={key: [30] * 2 for key in hetero_data.edge_types},
+    # Use a batch size of 128 for sampling training nodes of type paper
+    batch_size=128,
+    input_nodes=('paper', hetero_data['paper'].train_mask),
+)
+
+sampled_hetero_data = next(iter(loader))
+print(sampled_hetero_data['paper'].batch_size)
+
+print(torch.__version__)
+
+# print(pyg_lib.__version__)
+
 
